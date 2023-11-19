619fba01a43adfb7d8cdd9635796bb0569b16fe3
Jocelyn Chan <jocelyn.chan042@gmail.com>
2023-11-19 10:12:35 -0500

    vnfeio

--- roadtrip/main.js
+++ roadtrip/main.js
@@ -3,13 +3,13 @@ let linkTwo = document.getElementById("link-two");
 
 function changeVariableOnClick(id) {
   var rand = Math.random();
-  if (id === 1) {
+  if (id === "link-one") {
     if (rand <= .7) {
       window.location.href = 'win.html';
     } else {
       window.location.href = 'lose.html';
     }
-  } else if (id === 2) {
+  } else if (id === "link-two") {
     if (rand <= .4) {
       window.location.href = 'win.html';
     } else {
@@ -18,27 +18,11 @@ function changeVariableOnClick(id) {
   } 
 }
 
-/*
+
 linkOne.addEventListener('click', function() {
-  changeVariableOnClick('link-one');
+  changeVariableOnClick("link-one");
 });
 
 linkTwo.addEventListener('click', function() {
-  changeVariableOnClick(2);
+  changeVariableOnClick("link-two");
 });
-
-linkThree.addEventListener('click', function() {
-    changeVariableOnClick('link-three');
-  });
-
-linkFour.addEventListener('click', function() {
-    changeVariableOnClick('link-four');
-  });
-
-  linkFive.addEventListener('click', function() {
-    changeVariableOnClick('link-five');
-  });
-
-  linkSix.addEventListener('click', function() {
-    changeVariableOnClick('link-six');
-  });
